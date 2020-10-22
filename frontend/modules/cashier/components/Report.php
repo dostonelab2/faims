1a99b43be7e2e42b129b9e31096f8efbe4a9377b
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2020-10-22 11:44:07 +0800

    Update Report.php

--- frontend/modules/cashier/components/Report.php
+++ frontend/modules/cashier/components/Report.php
@@ -131,7 +131,7 @@ class Report {
             $template .= "<tr>";
             $template .= "<td style='border-bottom: 1px solid #000; border-right: 1px solid #000; padding-left:  5px;'>".$item->name."</td>";
             $template .= "<td style='text-align: center; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".$item->creditor->account_number."</td>";
-            $template .= "<td style='text-align: center; border-bottom: 1px solid #000; border-right: 1px solid #000; font-size: x-small;'>".$item->osdv->os->os_number."</td>";
+            $template .= "<td style='text-align: center; border-bottom: 1px solid #000; border-right: 1px solid #000; font-size: x-small;'>".($item->osdv->os ? $item->osdv->os->os_number : $item->osdv->dv->dv_number)."</td>";
             $template .= "<td style='text-align: center; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".($item->osdv->uacs ? $item->osdv->uacs->expenditureobject->object_code : '-')."</td>";
             $template .= "<td style='text-align: right; padding-right: 10px; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".number_format($item->osdv->getGrossamount(),2)."</td>";
             $template .= "<td style='text-align: right; padding-right: 10px; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".number_format($item->osdv->getTax(),2)."</td>";
