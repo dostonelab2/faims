299c8aaefa8fdf173f58b3beffbbec5521513fcf
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2020-11-11 13:02:31 +0800

    Update Osdv.php

--- common/models/finance/Osdv.php
+++ common/models/finance/Osdv.php
@@ -197,6 +197,21 @@ class Osdv extends \yii\db\ActiveRecord
         $tax_amount = 0.00;
                         
         switch ($model->tax_category_id) {
+          case 0:
+            if($model->osdv->request->creditor->tagged || $model->tax_registered)
+                    $taxable_amount = round($model->amount / 1.12, 2);
+                else
+                    $taxable_amount = $model->amount;
+
+                if($model->osdv->request->creditor->tagged || $model->amount > 10000.00){
+                    $tax1 = round($taxable_amount * $model->rate1, 2);
+                    $tax2 = round($taxable_amount * $model->rate2, 2);
+                    $tax_amount = $tax1 + $tax2;
+                }else{
+                    $tax_amount = round($taxable_amount * $model->rate1, 2);
+                }
+            break;        
+            
           case 1: //Goods (5% + 1%); check if 10k above; if below 10 check if supplier is tagged(amount divide by 1.12%)
                 if($model->osdv->request->creditor->tagged || $model->tax_registered)
                     $taxable_amount = round($model->amount / 1.12, 2);
@@ -235,19 +250,7 @@ class Osdv extends \yii\db\ActiveRecord
                 $tax_amount = round($model->amount * $model->rate1, 2);
             break;
 
-          default:
-            if($model->osdv->request->creditor->tagged || $model->tax_registered)
-                    $taxable_amount = round($model->amount / 1.12, 2);
-                else
-                    $taxable_amount = $model->amount;
-
-                if($model->osdv->request->creditor->tagged || $model->amount > 10000.00){
-                    $tax1 = round($taxable_amount * $model->rate1, 2);
-                    $tax2 = round($taxable_amount * $model->rate2, 2);
-                    $tax_amount = $tax1 + $tax2;
-                }else{
-                    $tax_amount = round($taxable_amount * $model->rate1, 2);
-                }
+          
         }
 
         return $model->amount - $tax_amount;
