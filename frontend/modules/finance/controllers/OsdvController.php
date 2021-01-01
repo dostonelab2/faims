c528d18eb1ac8fa0d0d957fbae7050a792ee36c1
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2021-01-01 14:40:27 +0800

    Update OsdvController.php

--- frontend/modules/finance/controllers/OsdvController.php
+++ frontend/modules/finance/controllers/OsdvController.php
@@ -495,7 +495,8 @@ class OsdvController extends Controller
                             $os->osdv_id = $model->osdv_id;
                             $os->request_id = $model->request->request_id;
                             $os->os_number = Os::generateOsNumber($model->expenditure_class_id, date("Y-m-d H:i:s"));
-                            $os->os_date = date("Y-m-d", strtotime($model->create_date));
+                            //$os->os_date = date("Y-m-d", strtotime($model->create_date));
+                            $os->os_date = date("Y-m-d H:i:s");
                             $os->save(false);
                         }
                         
@@ -550,7 +551,8 @@ class OsdvController extends Controller
                             $dv->request_id = $model->request->request_id;
                             $dv->obligation_type_id = $model->request->obligation_type_id;
                             $dv->dv_number = Dv::generateDvNumber($model->request->obligation_type_id, $model->expenditure_class_id, date("Y-m-d H:i:s"));
-                            $dv->dv_date = date("Y-m-d", strtotime($model->create_date));
+                            //$dv->dv_date = date("Y-m-d", strtotime($model->create_date));
+                            $dv->dv_date = date("Y-m-d H:i:s");
                             $dv->save(false);
                         }
                         
