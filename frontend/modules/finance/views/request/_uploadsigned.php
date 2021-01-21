e3a3dcd9f83ba7c20279ec4caa961dd37eb63bac
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2021-01-21 10:44:57 +0800

    Update _uploadsigned.php

--- frontend/modules/finance/views/request/_uploadsigned.php
+++ frontend/modules/finance/views/request/_uploadsigned.php
@@ -21,7 +21,8 @@ echo $form->field($model, 'pdfFile')->widget(FileInput::classname(), [
         //'initialPreview' =>[Requestattachment::checkFile($model->attachment_id)],
         'initialPreview' => [
             //Yii::getAlias('@uploads') . "finance/request/" . $model->request->request_number. "/" . $model->filename,
-            "/uploads/finance/request/" . $model->requestattachment->request->request_number. "/" .$model->request_attachment_id. $model->filename,
+            //"/uploads/finance/request/" . $model->requestattachment->request->request_number. "/" .$model->request_attachment_id. $model->filename,
+            "/uploads/finance/request/" . $model->requestattachment->request->request_number. "/" . $model->filename,
         ],
         'initialPreviewAsData'=>true,
         'initialPreviewConfig'=>[
