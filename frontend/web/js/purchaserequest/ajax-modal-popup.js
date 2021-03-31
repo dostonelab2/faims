c5dd75db890c715bcaf52b4d05a31e09020393b4
idontwindows <eduzaragozajr@gmial.com>
2021-03-31 14:14:41 +0800

    new purchase request module

---
+++ frontend/web/js/purchaserequest/ajax-modal-popup.js
@@ -0,0 +1,9 @@
+$("body").on("click", "#buttonCreatePR", function() {
+    $('#createPRModal').modal('show')
+        .find('#modalContent')
+        .load($(this).attr('value'));
+    $('#modalHeader').html($(this).attr('title'));
+    setTimeout(function() {
+        $("#btnrefresh").click();
+    }, 1500);
+});
