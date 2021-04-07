0ee6c0032380d000bc5603e37986280d9ca4111f
idontwindows <eduzaragozajr@gmial.com>
2021-04-07 13:37:49 +0800

    update change-password form

--- common/modules/admin/views/user/change-password.php
+++ common/modules/admin/views/user/change-password.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
@@ -10,20 +11,22 @@ $this->title = Yii::t('rbac-admin', 'Change Password');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to change password:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'form-change']); ?>
-                <?= $form->field($model, 'oldPassword')->passwordInput() ?>
-                <?= $form->field($model, 'newPassword')->passwordInput() ?>
-                <?= $form->field($model, 'retypePassword')->passwordInput() ?>
-                <div class="form-group">
-                    <?= Html::submitButton(Yii::t('rbac-admin', 'Change'), ['class' => 'btn btn-primary', 'name' => 'change-button']) ?>
+    <div class="panel panel-default col-xs-12">
+        <div class="panel-heading"><i class="fa fa-user-circle fa-adn"></i><?= ' ' . Html::encode($this->title) ?></div>
+        <div class="panel-body">
+        <p>Please fill out the following fields to change password:</p>
+            <div class="row">
+                <div class="col-lg-5">
+                    <?php $form = ActiveForm::begin(['id' => 'form-change']); ?>
+                    <?= $form->field($model, 'oldPassword')->passwordInput() ?>
+                    <?= $form->field($model, 'newPassword')->passwordInput() ?>
+                    <?= $form->field($model, 'retypePassword')->passwordInput() ?>
+                    <div class="form-group">
+                        <?= Html::submitButton(Yii::t('rbac-admin', 'Change'), ['class' => 'btn btn-primary', 'name' => 'change-button']) ?>
+                    </div>
+                    <?php ActiveForm::end(); ?>
                 </div>
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
