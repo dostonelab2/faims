d13266d840278fee069e3439e5d63539e20bdb58
arismoratalla <arismoratalla@gmail.com>
2021-05-05 13:58:54 +0800

    Update index.php

--- frontend/modules/procurement/views/bids/index.php
+++ frontend/modules/procurement/views/bids/index.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 use common\components\Functions;
 
 $func = new Functions();
-
+echo $BaseURL;
 
 $BaseURL = $GLOBALS['frontend_base_uri'];
 $this->title = 'Quotation Bids and Awards';
