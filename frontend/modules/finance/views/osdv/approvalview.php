7aa96a08afcfc9acce748c2c42b27b9a44b9d6b0
arismoratalla <arismoratalla@gmail.com>
2021-06-01 10:39:33 +0800

    Update approvalview.php

--- frontend/modules/finance/views/osdv/approvalview.php
+++ frontend/modules/finance/views/osdv/approvalview.php
@@ -153,6 +153,7 @@ Modal::end();
             [
                 'attribute'=>'type_id',
                 'label'=>'Fund Source',
+                'visible' => false,
                 'labelColOptions'=>['style'=>'width:35%; text-align: right;'],
                 'inputContainer' => ['class'=>'col-sm-2'],
                 'value' => $model->type->name,
@@ -347,7 +348,7 @@ Modal::end();
                 'headerOptions' => ['style' => 'text-align: center; width: 10px;'],
                 'pageSummary' => '',  
             ],
-            [   
+            /*[   
                 'attribute'=>'creditor_id',
                 'header' => 'Name',
                 'headerOptions' => ['style' => 'text-align: center;'],
@@ -358,22 +359,56 @@ Modal::end();
                     return $model->creditor->name;
                 },
                 
-            ],
+            ],*/
             [
                 'attribute'=>'particulars',
                 'header'=>'Particulars',
-                'width'=>'350px',
+                'width'=>'650px',
                 'value'=>function ($model, $key, $index, $widget) { 
-                        return $model->particulars;
+                        return '<span style="text-align: left; font-weight: bold; float: left; color: black;">'.$model->creditor->name.'</span><br/><p style="text-align: left; float: left;">'.$model->particulars.'</p>';
                     },
+                'format'=>'raw',
                 'headerOptions' => ['style' => 'text-align: center'],
-                'contentOptions' => ['style' => 'padding-right: 20px;'],
+                'contentOptions' => [
+                    'style'=>'max-width:150px; overflow: auto; white-space: normal; word-wrap: break-word;'
+                ],
                 'hAlign'=>'left',
                 'vAlign'=>'left',
                 'width'=>'800px',
                 'pageSummary' => 'TOTAL', 
                 'pageSummaryOptions' => ['style' => 'text-align: left;'],
             ],
+            /*[
+                'class'=>'kartik\grid\EditableColumn',
+                'attribute'=>'particulars',
+                'header'=>'Particulars',
+                'refreshGrid'=>true,
+                'width'=>'650px',
+                'contentOptions' => [
+                    'style'=>'max-width:150px; overflow: auto; white-space: normal; word-wrap: break-word;'
+                ],
+                'value'=>function ($model, $key, $index, $widget) { 
+                        return '<span style="text-align: left; font-weight: bold; float: left; color: black;">'.$model->creditor->name.'</span><br/><p style="text-align: left; float: left;">'.$model->particulars.'</p>';
+                    },
+                'format'=>'raw',
+                'editableOptions'=> function ($model , $key , $index) {
+                    return [
+                        'options' => ['id' => $index . '_' . $model->request_payroll_id],
+                        'contentOptions' => ['style' => 'padding-right: 20px; text-align: left;'],
+                        'placement'=>'bottom',
+                        //'disabled'=>!Yii::$app->user->can('access-finance-disbursement'),
+                        'name'=>'amount',
+                        'asPopover' => true,
+                        'value' => $model->particulars,
+                        'inputType' => \kartik\editable\Editable::INPUT_TEXTAREA,
+                        'size'=>'lg',
+                        'options' => ['class'=>'form-control', 'rows'=>5, 'placeholder'=>'Enter particulars...'],
+                        'formOptions'=>['action' => ['/finance/requestpayroll/updateamount']], // point to the new action
+                    ];
+                },
+                'pageSummary' => 'TOTAL', 
+                'pageSummaryOptions' => ['style' => 'text-align: right; padding-right:30px;'],
+            ],*/
             [
                 'attribute'=>'amount',
                 'header'=>'Amount',
@@ -384,7 +419,7 @@ Modal::end();
                         return $model->amount;
                     },
                 'headerOptions' => ['style' => 'text-align: center'],
-                'contentOptions' => ['style' => 'padding-right: 20px;'],
+                'contentOptions' => ['style' => 'padding-right: 20px; vertical-align: middle; '],
                 'hAlign'=>'right',
                 'vAlign'=>'left',
                 'width'=>'250px',
