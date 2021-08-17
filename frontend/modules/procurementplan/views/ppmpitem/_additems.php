94c2c5aa09fbb884b45becae284fba3e65308a45
idontwindows <63185758+idontwindows@users.noreply.github.com>
2021-08-17 13:51:57 +0800

    update unit type

--- frontend/modules/procurementplan/views/ppmpitem/_additems.php
+++ frontend/modules/procurementplan/views/ppmpitem/_additems.php
@@ -86,7 +86,7 @@ use common\models\procurementplan\Unitofmeasure;
             [
                 'attribute' => 'unit_of_measure_id',
                 'value'=>function ($model, $key, $index, $widget){ 
-                            return $model->getUnit();
+                            return $model->getUnittype();
                         },
             ],
             //'price_catalogue',
--- frontend/modules/procurementplan/views/supplemental/_additem.php
+++ frontend/modules/procurementplan/views/supplemental/_additem.php
@@ -126,7 +126,7 @@ use yii\widgets\Pjax;
                 'attribute' => 'unit_of_measure_id',
                 'header' => 'UoM',
                 'value'=>function ($model, $key, $index, $widget){ 
-                            return $model->getUnit();
+                            return $model->getUnittype();
                         },
             ],
             //'price_catalogue',
