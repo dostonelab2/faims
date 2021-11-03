34441bdfa21289358aa9a6863a43978ee4dde13b
arismoratalla <arismoratalla@gmail.com>
2021-11-03 12:55:35 +0800

    Report of Disbursement Updates
    added request date range filter

--- frontend/modules/finance/views/osdv/_report.php
+++ frontend/modules/finance/views/osdv/_report.php
@@ -44,8 +44,8 @@ Modal::end();
 /****Date range picker *****/
 $DateRangePicker = function(){
     $daterange =  DateRangePicker::widget([
-        'name'=>'date_range_1',
-        'value'=>'01-Jan-21 to 20-Feb-21',
+        'name'=>'request_date',
+        'value'=>'',
         'options' => [
             'class' => 'form-control',
             'id' => 'date-range'
@@ -61,19 +61,13 @@ $DateRangePicker = function(){
             ],
             'opens'=>'left'
         ]
+        
     ]) . '<i class="fas fa-calendar-alt icon"></i>';
 
     return '<div class="input-group input-container drp-container">'. $daterange .'</div>';
 };
 ?>
 <div class="request-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <!--?= Html::a('Create', ['create'], ['class' => 'btn btn-success', 'id' => 'buttonCreateRequest']) ?-->
-    </p>
 <?php Pjax::begin(); ?>
       <?php
         echo GridView::widget([
@@ -86,29 +80,30 @@ $DateRangePicker = function(){
                                 'attribute'=>'request_date',
                                 'headerOptions' => ['style' => 'text-align: center;'],
                                 'contentOptions' => ['style' => 'text-align: center;'],
-                                'width'=>'150px',
+                                'width'=>'350px',
                                 'value'=>function ($model, $key, $index, $widget) { 
                                     return date('Y-m-d', strtotime($model->request_date));
                                 },
-                                /*'filterType' => GridView::FILTER_DATE_RANGE,
+                                //'filterType' => $DateRangePicker(),
+                                'hAlign' => GridView::ALIGN_CENTER,
+                                'filterType' => GridView::FILTER_DATE_RANGE,
                                     'value' => function($model) {
                                         if ($model->request_date) {
-                                            return date('Y-m-d H:i:s',$model->request_date);
+                                            return date('Y-m-d', strtotime($model->request_date));
                                         }
                                         return null;
                                     },
                                     'filterWidgetOptions' => [
-                                        'startAttribute' => 'request_date_c', //Attribute of start time
+                                        'startAttribute' => 'request_date_s', //Attribute of start time
                                         'endAttribute' => 'request_date_e',   //The attributes of the end time
                                         'convertFormat'=>true, // Importantly, true uses the local - > format time format to convert PHP time format to js time format.
                                         'pluginOptions' => [
-                                            'format' => 'yyyy-mm-dd hh:ii:ss',//Date format
-                                              'timePicker'=>true, //Display time
-                        //                        'Time Picker Increment'=>5, //min interval
-                                             'timePicker24Hour' => true, //24 hour system
-                                            'locale'=>['format' => 'Y-m-d H:i:s'], //php formatting time
+                                            'format' => 'yyyy-mm-dd',//Date format
+                                            'timePicker'=>true, //Display time
+                                            'timePicker24Hour' => true, //24 hour system
+                                            'locale'=>['format' => 'Y-m-d'], //php formatting time
                                         ]
-                                    ],*/
+                                    ],
                             ],
                             [
                                 'attribute'=>'payee_id',
@@ -139,54 +134,6 @@ $DateRangePicker = function(){
                                 ],  
                                 'filterInputOptions' => ['placeholder' => 'Select Payee']
                             ],
-                            /*[
-                                'attribute'=>'payee_id',
-                                'contentOptions' => ['style' => 'padding-left: 25px; font-weigth: bold;'],
-                                'width'=>'550px',
-                                'contentOptions' => [
-                                    'style'=>'max-width:300px; overflow: auto; white-space: normal; word-wrap: break-word;'
-                                ],
-                                'format' => 'raw',
-                                'value'=>function ($model, $key, $index, $widget) { 
-                                    return '<b>' . Creditor::findOne($model->payee_id)->name. '</b><br>' .$model->particulars;
-                                },
-                            ],
-                            
-                            [
-                                'attribute'=>'osdv_id',
-                                'header'=>'OS Number',
-                                'headerOptions' => ['style' => 'text-align: center;'],
-                                'contentOptions' => ['style' => 'text-align: center;'],
-                                'width'=>'150px',
-                                'format'=>'raw',
-                                'value'=>function ($model, $key, $index, $widget) { 
-                                    return isset($model->os->os_id) ? '<b>'.$model->os->os_number.'</b>' : '';
-                                },
-                                'filterType' => GridView::FILTER_SELECT2,
-                                'filter' => ArrayHelper::map(Os::find()->orderBy(['os_id' => SORT_DESC])->asArray()->all(), 'os_id', 'os_number'), 
-                                'filterWidgetOptions' => [
-                                    'pluginOptions' => ['allowClear' => true],
-                                ],  
-                                'filterInputOptions' => ['placeholder' => 'Select OS'],
-                            ],
-                            [
-                                'attribute'=>'osdv_id',
-                                'header'=>'DV Number',
-                                'headerOptions' => ['style' => 'text-align: center;'],
-                                'contentOptions' => ['style' => 'text-align: center;'],
-                                'width'=>'150px',
-                                'format'=>'raw',
-                                'value'=>function ($model, $key, $index, $widget) { 
-                                    return isset($model->dv->dv_id) ? '<b>'.$model->dv->dv_number.'</b>' : '';
-                                    //return $model->dv->dv_number;
-                                },
-                                'filterType' => GridView::FILTER_SELECT2,
-                                'filter' => ArrayHelper::map(Dv::find()->orderBy(['dv_id' => SORT_DESC])->asArray()->all(), 'dv_id', 'dv_number'), 
-                                'filterWidgetOptions' => [
-                                    'pluginOptions' => ['allowClear' => true],
-                                ],  
-                                'filterInputOptions' => ['placeholder' => 'Select DV'],
-                            ],*/
                             [
                                 'attribute'=>'os_id',
                                 'header'=>'OS Number',
@@ -209,7 +156,6 @@ $DateRangePicker = function(){
                                         $label = 'label-warning';
                                     }
                                     
-                                    //return isset($model->osdv->os) ? '<b>'.$model->osdv->os->os_number.'</b><br/>'.date('Y-m-d', strtotime($model->osdv->os->os_date)) : '';
                                     return (isset($model->osdv->os) ? '<span class="label '.$label.'">'.$model->osdv->os->os_number.'</span><br/>'.$model->osdv->os->os_date : '');
                                 },
                                 'filterType' => GridView::FILTER_SELECT2,
@@ -270,7 +216,6 @@ $DateRangePicker = function(){
                                 'format'=>['decimal',2],
                                 'value'=>function ($model, $key, $index, $widget) { 
                                     return $model->osdv->getTax();
-                                    //return '<b>'.$model->request->amount.'</b>';
                                 },
                             ],
                             [
@@ -282,7 +227,6 @@ $DateRangePicker = function(){
                                 'format'=>['decimal',2],
                                 'value'=>function ($model, $key, $index, $widget) {
                                     return $model->osdv->getGrossamount();
-                                    //return '<b>'.$model->request->amount.'</b>';
                                 },
                             ],
                     ],
@@ -290,43 +234,11 @@ $DateRangePicker = function(){
             'headerRowOptions' => ['class' => 'kartik-sheet-style'],
             'filterRowOptions' => ['class' => 'kartik-sheet-style'],
             'pjax' => true, // pjax is set to always true for this demo
-            /*'rowOptions' => function($model){
-                switch ($model->status_id) {
-                    case Request::STATUS_VALIDATED:
-                        return ['class'=>'warning'];
-                        break;
-                    case Request::STATUS_CERTIFIED_ALLOTMENT_AVAILABLE:
-                        return ['class'=>'warning'];
-                        break;
-                    case Request::STATUS_ALLOTTED:
-                        return ['class'=>'warning'];
-                        break;
-                    case Request::STATUS_CERTIFIED_FUNDS_AVAILABLE:
-                        return ['class'=>'warning'];
-                        break;
-                    case Request::STATUS_CHARGED:
-                        return ['class'=>'warning'];
-                        break;
-                    case Request::STATUS_APPROVED_FOR_DISBURSEMENT:
-                        return ['class'=>'success'];
-                        break;
-                }
-                 
-            },*/
             'panel' => [
                     'heading' => '',
                     'type' => GridView::TYPE_PRIMARY,
-                    'before'=>Html::button('Validated Requests  &nbsp;&nbsp;<span class="badge badge-light"></span>', ['value' => Url::to(['osdv/create']), 'title' => 'Request', 'class' => 'btn btn-success', 'style'=>'margin-right: 6px;', 'id'=>'buttonCreateOsdv']),/*.' '.
-                    DateRangePicker::widget([
-                        'model'=>$model,
-                        'attribute'=>'datetime_range',
-                        'convertFormat'=>true,
-                        'pluginOptions'=>[
-                            'timePicker'=>true,
-                            'timePickerIncrement'=>30,
-                            'format'=>'Y-m-d h:i A'
-                        ]
-                    ]),*/
+                    'before'=>'',//'<h4 style="margin-top: -3px;"><span class="label label-info">Filter visible data using the Request Date range.</span></h4>',
+                
                     'after'=>false,
                 ],
             
@@ -335,9 +247,7 @@ $DateRangePicker = function(){
                         [
                             [
                                 'content'=> $DateRangePicker() //Date Range Picker
-                                    /*Html::button('PENDING', ['title' => 'Approved', 'class' => 'btn btn-warning', 'style'=>'width: 90px; margin-right: 6px;']) .    
-                                    Html::button('SUBMITTED', ['title' => 'Approved', 'class' => 'btn btn-primary', 'style'=>'width: 90px; margin-right: 6px;']) .
-                                    Html::button('APPROVED', ['title' => 'Approved', 'class' => 'btn btn-success', 'style'=>'width: 90px; margin-right: 6px;'])*/
+                                   
                             ],
                             //'{export}',
                             //'{toggleData}'
