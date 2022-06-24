1b31f75220c8973e4312ed20327f01e247853de5
arismoratalla <arismoratalla@gmail.com>
2022-06-24 11:44:18 +0800

    backup for June

--- frontend/modules/finance/controllers/RequestController.php
+++ frontend/modules/finance/controllers/RequestController.php
@@ -795,7 +795,16 @@ class RequestController extends Controller
                     $content .= 'Particulars: '.PHP_EOL.$model->particulars;
                     $recipient = Notificationrecipient::find()->where(['status_id' => $model->status_id])->one();
                     
-                    Yii::$app->Notification->sendSMS('', 2, $recipient->primary->sms.','.$recipient->secondary->sms, 'Request for Verification', $content, 'FAIMS', $this->module->id, $this->action->id);
+                    Yii::$app->Notification->sendSMS(
+                        '', 
+                        2, 
+                        $recipient->primary->sms.','
+                        .$recipient->secondary->sms, 
+                        'Request for Verification', 
+                        $content, 
+                        'FAIMS', 
+                        $this->module->id, 
+                    $this->action->id);
                     
                     Yii::$app->Notification->sendEmail('', 2, $recipient->primary->email.','.$recipient->secondary->email, 'Request for Verification', $content, 'FAIMS', $this->module->id, $this->action->id);
                     
